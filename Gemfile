9b829fe77d69c5f8e8c136915bd19c7dcf54afb8
Phil Monroe <phil@philmonroe.com>
2014-01-29 15:32:45 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.2'
+
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jbuilder', '~> 1.2'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rack-timeout'
+gem 'sass-rails', '~> 4.0.0'
+gem 'turbolinks'
+gem 'uglifier', '>= 1.3.0'
+gem 'unicorn'
+
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kegup::Application.config.secret_key_base = '643a8be03076d238ab48d7f531ab904f276f7603e5e7aa06a1e3964d84b148a88b313ec60fc3f51712de46b2086f2d8e5c9ca2faf9742369e031522bd2220904'
