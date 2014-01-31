9466177fe5a19f7d80ef91976b1328bdec9bd65d
Phil Monroe <phil@philmonroe.com>
2014-01-31 10:45:46 -0800

    always redirect to an org for now on root

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -2,6 +2,8 @@ class WebsiteController < ApplicationController
   def index
     if current_user && current_user.orgs.present?
       redirect_to current_user.orgs.first
+    else
+      redirect_to Org.first
     end
   end
 end
