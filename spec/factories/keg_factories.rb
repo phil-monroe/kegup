c9ec6ca9977ca7b05403d748ef6dd8a35627a004
Phil Monroe <phil@philmonroe.com>
2014-01-30 20:07:07 -0800

    cleaning up tests and adding spec for tap

---
+++ spec/factories/keg_factories.rb
@@ -0,0 +1,4 @@
+FactoryGirl.define do
+  factory :keg do
+  end
+end
--- spec/models/tap_spec.rb
+++ spec/models/tap_spec.rb
@@ -12,4 +12,49 @@ describe Tap do
     tap1.save
     expect(build(:tap, org_id: 1, name: 'tap 1')).to_not be_valid
   end
+
+  describe 'modifying kegs' do
+    let(:keg1) { create(:keg) }
+    let(:keg2) { create(:keg) }
+    let(:tap){ create(:tap) }
+
+    it 'updates new kegs tapped date' do
+      expect(keg1.tapped_date).to be_nil
+      expect(keg1.finished_date).to be_nil
+      expect(keg2.tapped_date).to be_nil
+      expect(keg2.finished_date).to be_nil
+
+      tap.keg = keg1
+      tap.save
+      keg1.reload
+      keg2.reload
+
+      expect(keg1.tapped_date).to be_present
+      expect(keg1.finished_date).to be_nil
+      expect(keg2.tapped_date).to be_nil
+      expect(keg2.finished_date).to be_nil
+
+      tap.keg = keg2
+      tap.save
+      keg1.reload
+      keg2.reload
+
+      expect(keg1.tapped_date).to be_present
+      expect(keg1.finished_date).to be_present
+      expect(keg2.tapped_date).to be_present
+      expect(keg2.finished_date).to be_nil
+
+      tap.keg = nil
+      tap.save
+      keg1.reload
+      keg2.reload
+
+      expect(keg1.tapped_date).to be_present
+      expect(keg1.finished_date).to be_present
+      expect(keg2.tapped_date).to be_present
+      expect(keg2.finished_date).to be_present
+    end
+
+  end
+
 end
