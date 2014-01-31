7fb94c3f15d7ced007e61f6d1757bc050a8ca16f
Evgueni Ratchev <evgueni@identified.com>
2014-01-30 17:12:16 -0800

    seeding more beers :)

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,5 @@
 class Beer < ActiveRecord::Base
-  STYLES = ['IPA', 'Amber', 'Lager', 'Pale Ale', 'Marzen', 'Altbier', 'American Pale Ale', 'Porter', 'California Common / Steam', 'Amber Ale', 'Witbier', 'Abbey Dubbel', 'Imperial IPA', 'Pale Lager', 'Strong Ale']
+  STYLES = ['IPA', 'Amber', 'Lager', 'Pale Ale', 'Marzen', 'Altbier', 'American Pale Ale', 'Porter', 'California Common / Steam', 'Amber Ale', 'Witbier', 'Abbey Dubbel', 'Imperial IPA', 'Pale Lager', 'Strong Ale', 'English Pale Ale', 'Marzen / Oktoberfest', 'Bohemian / Czech Pilsener', 'Hefeweizen', 'Cider']
   validates :name, presence: true
   validates :style, inclusion: { in: STYLES }
 
