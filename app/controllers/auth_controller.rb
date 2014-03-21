2d5f7ff35ea14948bad72750c91142f17cddebaf
Phil Monroe <phil@philmonroe.com>
2014-03-20 21:35:22 -0700

    adding a friendly reminder to have users fill out my beers after login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,5 @@
 class AuthController < ApplicationController
+  after_filter :my_beers_reminder, only: :facebook
 
   def facebook
     user = User.from_facebook(auth_hash)
@@ -14,5 +15,11 @@ class AuthController < ApplicationController
   private
   def auth_hash
     request.env['omniauth.auth']
+  end
+
+  def my_beers_reminder
+    if current_user.favorite_beers.count == 0
+      flash[:info] = "Head over to <b>'#{view_context.link_to('My Beers', users_favorite_beers_path)}'</b> to get your favorite beers suggested to your friendly neighborhood kegmeisters!".html_safe
+    end
   end
 end
