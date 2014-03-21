9a364a53bf4d5167031bc378ef26c8a63df2f9d0
Phil Monroe <phil@philmonroe.com>
2014-03-20 19:44:49 -0700

    fix assets bug from slate upgrade and cleanup assets

---
+++ app/assets/javascripts/application.coffee
@@ -0,0 +1,2 @@
+#= require slate
+#= require orgs
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  layout 'slate'
 
   def login user
     session[:user_id] = user.id
