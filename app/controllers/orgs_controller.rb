9f0e9b4ee35a384f2b043b80774b024bafbe7ba8
Phil Monroe <phil@philmonroe.com>
2014-03-20 19:45:19 -0700

    cleanup orgs index and add org create

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -1,9 +1,29 @@
 class OrgsController < ApplicationController
   def index
-    @orgs = Org.all
+    @orgs = Org.order('updated_at DESC').all
   end
 
   def show
     @org = Org.find(params[:id])
+  end
+
+  def new
+    @org = Org.new
+  end
+
+  def create
+    org = Org.new(org_params)
+    if org.save
+      org.org_user_memberships.create user: current_user, is_kegmeister: true
+      flash[:success] = "Successfully created #{org.name}"
+      redirect_to org
+    else
+      render :new
+    end
+  end
+
+  private
+  def org_params
+    params.require(:org).permit(:name)
   end
 end
