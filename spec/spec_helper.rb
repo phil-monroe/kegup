e52f079ffeabaa997106bd78fde67fae980dd0a2
Phil Monroe <phil@philmonroe.com>
2014-04-17 23:52:08 -0700

    actually coverage test code in the lib folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,6 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 SimpleCov.start 'rails' do
   add_filter '/spec/'
   add_filter '/config/'
-  add_filter '/lib/'
   add_filter '/vendor/'
   add_filter '/.bundle/'
 
