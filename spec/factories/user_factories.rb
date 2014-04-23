0d11ba67c04b400b8447d3774f4aa26bbc615fed
Ricky Chilcott <ricky@rbsavvy.com>
2014-04-22 23:01:22 -0400

    Adding Favorite Beer specs

--- spec/factories/user_factories.rb
+++ spec/factories/user_factories.rb
@@ -3,5 +3,13 @@ FactoryGirl.define do
     sequence(:first_name) {|n| "first-#{n}" }
     sequence(:last_name) {|n| "last-#{n}" }
     sequence(:email) {|n| "email-#{n}@test.com" }
+
+    ignore do
+      favorite_beer_count 3
+    end
+
+    after(:create) do |user, evaluator|
+      create_list(:favorite_beer, evaluator.favorite_beer_count, user: user)
+    end
   end
 end
